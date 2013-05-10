74acd542cdfb03b82657b87401b0f2a451c45aab
Alex Wollman <alex.wollman@gmail.com>
2013-05-10 16:55:07 -0500

    everything should be going through tig, but just in case pushing through old school git

--- nodePlay.js
+++ nodePlay.js
@@ -1,2 +1,14 @@
 var fs = require('fs');
+var data = null;
 
+//sample reading of file and outputting with callbacks
+fs.readFile( 'database.json', 'ascii', function(err, data){
+    if(err){
+        console.log('Error: ' + err);
+    }
+    data = JSON.parse(data);
+    console.log("inside 'readfile' callback, cannot put 'data' in same line");
+    console.log(data);
+});
+
+console.log("outside 'readfile' callback, looks like would have to do callback chaining " + data);
